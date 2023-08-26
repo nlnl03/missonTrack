cf145aefaf654f4b7cdebae5d58073cb64c7ea05
nlnl03 <lavinoam2003@gmail.com>
2023-08-27 00:15:51 +0300

    new

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="app-header">
+      <div class="app-logo">
+        <img src="https://aws-amplify.github.io.images/Logos/Amplify-Logo-White.svg" alt="AWS Amplify">
+      </div>
+      <h1>Welcome to Amplify</h1>
+    </div>
+  </div>
+
+  <amplify-authenticator>
+    <div class="welcome">
+      <h1>Hey, {{ user.username }}</h1>
+      <amplify-sign-out></amplify-sign-out>
+    </div>
+
+    <div class="form-body">
+      <form @submit.prevent>
+        <button @click="getTodos" class="button">GET</button>
+      </form>
+    </div>
+  </amplify-authenticator>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+ import { AuthState, onAuthUIStateChange } from 'aws-amplify/ui-components'
+//  import {API} from 'aws-amplify'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      user:{}
+    }
+  },
+  created(){
+    onAuthUIStateChange((state, user) => {
+      if(state === AuthState.SignedIn){
+        this.user = user
+        console.log(user)
+      }
+    })
+  },
+  methods:{
+    getTodos(){
+      console.log("getTodos")
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { Amplify } from 'aws-amplify'
+import '@aws-amplify/ui-vue'
+import aws_exports from './aws-exports'
+
+Amplify.configure(aws_exports)
+
+// Vue.config.productionTip = false
+
 createApp(App).mount('#app')
